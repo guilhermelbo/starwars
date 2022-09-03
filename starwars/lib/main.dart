2c13d0da6146dde6a4df61466a547b6a9634b050
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-09-03 15:21:52 -0300

    Atualização da main

--- starwars/lib/main.dart
+++ starwars/lib/main.dart
@@ -1,202 +1,99 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:fluttermoji/fluttermoji.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:starwars/database/db.dart';
-import 'package:get/get.dart';
+import 'package:starwars/avatar_customize.dart';
+import 'package:starwars/avatar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fluttermoji Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'Star Wars App',
       theme: ThemeData.light(),
       // darkTheme: ThemeData.dark(),
-      home: MyHomePage(title: 'Fluttermoji'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<dynamic> futureSvg;
-  String svg = '';
-
-  _MyHomePageState() {
-    Get.put(FluttermojiController());
-  }
-
-  loadSvg() async {
-    var db = await DB.instance.database;
-    return await db.query('avatar');
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
+  Widget bodyContent = const Text('testando');
+  
+  changeBodyContent(){
+    setState(() {
+      bodyContent = const AvatarCustomize();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    futureSvg = loadSvg();
-    futureSvg.then((value) => {
-      setState((){
-        svg = value.first['svg'];
-      })
-    });
+    TabController tabController = TabController(length: 3, vsync: this);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-        centerTitle: true,
-      ),
-      body: ListView(
-        physics: BouncingScrollPhysics(),
+      backgroundColor: Colors.white,
+      body: Column(
         children: <Widget>[
           SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "Use your Fluttermoji anywhere\nwith the below widget",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: futureSvg,
-            builder:(context, snapshot){
-              if(svg != ''){
-                return SvgPicture.string(svg);
-              }
-              return Container();
-            }
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "and create your own page to customize them using our widgets",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-              textAlign: TextAlign.center,
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: OutlinedButton(
+                    onPressed: (){}, 
+                    child: const Text('Site Oficial')
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(right: 10, top: 10),
+                  child: TextButton(
+                    onPressed: ()=>{
+                      changeBodyContent()
+                    },
+                    child: const Avatar()
+                  ), 
+                ),
+              ],
             ),
           ),
           SizedBox(
             height: 50,
-          ),
-          Row(
-            children: [
-              Spacer(flex: 2),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  height: 35,
-                  child: ElevatedButton.icon(
-                    icon: Icon(Icons.edit),
-                    label: Text("Customize"),
-                    onPressed: () => Navigator.push(context,
-                        new MaterialPageRoute(builder: (context) => NewPage())),
-                  ),
-                ),
-              ),
-              Spacer(flex: 2),
-            ],
+            child: TabBar(
+              controller: tabController,
+              labelColor: Colors.black,
+              tabs: const [
+                Tab(text: 'Filmes'),
+                Tab(text: 'Personagens'),
+                Tab(text: 'Favoritos'),
+              ]
+            ),
           ),
           SizedBox(
-            height: 100,
-          ),
-        ],
-      ),
+            height: 150,
+            child: TabBarView(
+              controller: tabController,
+              children: const [
+                Tab(text: 'text'),
+                Tab(text: 'text 2'),
+                Tab(text: 'text 3'),
+              ]
+            ),
+          )
+        ]
+      )
     );
   }
 }
 
-class NewPage extends StatefulWidget{
-  NewPage({Key? key}) : super(key: key);
-
-  @override
-  _NewPageState createState() => _NewPageState();
-}
-
-class _NewPageState extends State<NewPage>{
-  late Future<dynamic> futureSvg;
-  String svg = '';
-
-  _NewPageState(){
-    futureSvg = loadSvg();
-    futureSvg.then((value) => {
-        svg = value.first['svg']
-    });
-  }
-
-  loadSvg() async {
-    var db = await DB.instance.database;
-    return await db.query('avatar');
-  }
-
-  salvar() async {
-    var db = await DB.instance.database;
-    db.update('avatar', {'svg': Get.find<FluttermojiController>().getFluttermojiFromOptions()});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var _width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30),
-                child: FluttermojiCircleAvatar(
-                  backgroundColor: Colors.grey[200],
-                  radius: 100,
-                ),
-              ),
-              SizedBox(
-                width: min(600, _width * 0.85),
-                child: Row(
-                  children: [
-                    Text(
-                      "Customize:",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    Spacer(),
-                    FluttermojiSaveWidget(
-                      onTap: salvar,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 30),
-                child: FluttermojiCustomizer(
-                  scaffoldWidth: min(600, _width * 0.85),
-                  autosave: false,
-                  theme: FluttermojiThemeData(
-                      boxDecoration: BoxDecoration(boxShadow: [BoxShadow()])),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
