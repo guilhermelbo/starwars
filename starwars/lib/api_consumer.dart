a85af0aa1f05eea0959c0ba4ed460121e0721361
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-09-04 19:22:17 -0300

    Melhorando a api consumer para buscar mais paginas

--- starwars/lib/api_consumer.dart
+++ starwars/lib/api_consumer.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiConsumer{
+  static int pageCharactersNumber = 1;
+
   static fetchMovies() async{
-    var url = Uri.https('swapi.dev', '/api/films/');
+    var url = Uri.parse('https://swapi.dev/api/films/');
     var response = await http.get(url);
     if (response.statusCode == 200){
       var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
@@ -17,7 +19,7 @@ class ApiConsumer{
   }
 
   static fetchCharacters() async{
-    var url = Uri.https('swapi.dev', '/api/people/');
+    var url = Uri.parse('https://swapi.dev/api/people/?page=$pageCharactersNumber');
     var response = await http.get(url);
     if (response.statusCode == 200){
       var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
@@ -25,7 +27,9 @@ class ApiConsumer{
       for (var element in jsonResponse['results']) {
         charactersName.add(element['name']);
       }
+      pageCharactersNumber++;
       return charactersName;
     }
+    return null;
   }
 }
